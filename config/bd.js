a4b25df4abebb222be35219cd680ab08ed04da69
zalda1518 <respaldos1992@gmail.com>
2026-02-05 20:21:27 -0500

    segundo commit se cuadra sequiler

--- config/bd.js
+++ config/bd.js
@@ -5,10 +5,10 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.DB_PORT,
 
   {
     host: process.env.DB_HOST,
+    port:process.env.DB_PORT,
     dialect: 'mysql',
     port: 3306,
     logging: false,
