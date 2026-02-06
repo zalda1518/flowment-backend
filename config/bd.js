beb6722e717cdd61259d7aa48271f8e7c4fb0389
zalda1518 <respaldos1992@gmail.com>
2026-02-05 20:44:30 -0500

    tercer commit

--- config/bd.js
+++ config/bd.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
 
   {
-    host: process.env.DB_HOST,
+    host: 'centerbeam.proxy.rlwy.net',
     port:process.env.DB_PORT,
     dialect: 'mysql',
     port: 3306,
