570ede16f3d96a6f8697c16e551ea7830ef3a0a7
zalda1518 <respaldos1992@gmail.com>
2026-02-05 20:07:01 -0500

    primer commit flowment

--- config/bd.js
+++ config/bd.js
@@ -2,11 +2,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'flowment',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  process.env.DB_PORT,
+
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306,
     logging: false,
